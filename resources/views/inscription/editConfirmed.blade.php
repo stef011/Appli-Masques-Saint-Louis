0b90dfcb1179ce85c3299f253e612f6cd5ceae57
Zundel Stéphane <stephane.zundel@viacesi.fr>
2020-05-20 15:29:12 +0200

    Removed QrCode

--- resources/views/inscription/editConfirmed.blade.php
+++ resources/views/inscription/editConfirmed.blade.php
@@ -8,7 +8,7 @@ Inscription Confirmée
 <div class="m-auto mw-10 text-center">
     <h2 class="h1 st-blue">Félicitations, votre inscription a bien été modifiée !</h2>
     <p class="h3 mb-3">Pour rappel, voici votre numéro d'inscription.</p>
-    {!! QrCode::size(250)->generate($inscription->numero) !!}
+    {{-- {!! QrCode::size(250)->generate($inscription->numero) !!} --}}
     <p class="h2 st-blue">{{ $inscription->numero }}</p>
     <p class="h3">Ce dernier est à conserver, il vous permettra de récupérer votre ou vos
         masques.</p>
--- resources/views/mail/confirmed.blade.php
+++ resources/views/mail/confirmed.blade.php
@@ -8,7 +8,7 @@ Confirmation de la demande
 <div class="m-auto mw-10 text-center">
     <h2 class="h1 st-blue">Félicitations, votre inscription a bien été prise en compte !</h2>
     <p class="h3 mb-3">Voici votre code unique d'inscription</p>
-    {!! QrCode::size(250)->generate($inscription->numero) !!}
+    {{-- {!! QrCode::size(250)->generate($inscription->numero) !!} --}}
     <p class="h2 st-blue">{{ $inscription->numero }}</p>
     <p class="h3">Ce dernier est à conserver, il vous permettra de récupérer votre ou
         vos
